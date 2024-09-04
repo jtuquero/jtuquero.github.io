336cd0cf43bf09e5fc22d3b6fc94bb0ea4d68fe2
jtuquero <168795006+jtuquero@users.noreply.github.com>
2024-09-04 16:04:10 +0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
 	"use strict";
-    
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : CarVilla HTML Template
-* Version       : 1.0
-==================================== */
-
-
-
-
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top
-2. welcome animation support
-3. owl carousel
-======================================*/
-
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
 			if ($(this).scrollTop() > 300) {
